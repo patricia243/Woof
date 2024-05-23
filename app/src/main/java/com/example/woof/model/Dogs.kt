d7ded8127e04e05ae57b2452e3a39ac0657368c9
patricia243 <21tb542@esisalama.org>
2024-05-23 10:28:16 +0200

    phase de couleur

---
+++ app/src/main/java/com/example/woof/model/Dogs.kt
@@ -0,0 +1,15 @@
+package com.example.woof.model
+
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+
+data class dogs(
+    val stringResourceId: Int,
+    val imageResourceId: Int,
+    @StringRes val stringResourceId: Int,
+    @DrawableRes val imageResourceId: Int
+
+)
+
+
+
